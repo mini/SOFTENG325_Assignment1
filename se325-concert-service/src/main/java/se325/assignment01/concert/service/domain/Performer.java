4308fcff936333ebe373ab41ead268f8c0919b12
Dhruv P <dpha900@aucklanduni.ac.nz>
2019-09-21 21:58:02 +1200

    Added empty request handlers

---
+++ se325-concert-service/src/main/java/se325/assignment01/concert/service/domain/Performer.java
@@ -0,0 +1,5 @@
+package se325.assignment01.concert.service.domain;
+
+public class Performer {
+
+}
--- se325-concert-service/src/main/java/se325/assignment01/concert/service/services/ConcertResource.java
+++ se325-concert-service/src/main/java/se325/assignment01/concert/service/services/ConcertResource.java
@@ -1,7 +1,84 @@
 package se325.assignment01.concert.service.services;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import se325.assignment01.concert.common.types.BookingStatus;
+
+@Path("/concert-service")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ConcertResource {
 
-    // TODO Implement this.
+	@GET
+	@Path("/concerts")
+	public Response getAllConcerts() {
+		
+		return null;
+	}
+	
+	@GET
+	@Path("/concerts/{id}")
+	public Response getConcert(@PathParam("id") long id) {
+		
+		return null;
+	}
+	
+	@GET
+	@Path("/concerts/summaries")
+	public Response getSummaries() {
+		
+		return null;
+	}
+	
+	@GET
+	@Path("/performers")
+	public Response getAllPerformers() {
+		
+		return null;
+	}
 
+	@GET
+	@Path("/performers/{id}")
+	public Response getPerformer(@PathParam("id") long id) {
+		
+		return null;
+	}
+	
+	@POST
+	@Path("/login")
+	public Response login() {
+		
+		return null;
+	}
+	
+	@POST
+	@Path("/bookings")
+	public Response makeBooking() {
+		
+		return null;
+	}
+	
+	@GET
+	@Path("/seats/{time}")
+	public Response getSeatsForConcert(@PathParam("time") String time, @DefaultValue("Any") @QueryParam("status") BookingStatus status) {
+		
+		return null;
+	}
+	
+	@POST
+	@Path("/subscribe/concertInfo")
+	public Response subscribeToConcert() {
+		
+		return null;
+	}
+	
 }
